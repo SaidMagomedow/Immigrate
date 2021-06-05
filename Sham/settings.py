1d9668ac2af818b90d8e0e431a83be868b31d236
SaidMagomedow <saidmagom@mail.ru>
2021-06-05 16:43:16 +0300

    Add CSS style

--- Sham/settings.py
+++ Sham/settings.py
@@ -75,13 +75,13 @@ WSGI_APPLICATION = 'Sham.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        'NAME': os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-        'USER': os.environ.get("SQL_USER", "user"),
-        'PASSWORD': os.environ.get("SQL_PASSWORD", "password"),
-        'HOST': os.environ.get("SQL_HOST", "localhost"),
-        'PORT': os.environ.get("SQL_PORT", "5432"),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('SQL_DATABASE', default='postgres'),
+        'USER': os.environ.get('SQL_USER', default='postgres'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', default='postgres'),
+        'HOST': os.environ.get('SQL_HOST', default='localhost'),
+        'PORT': "5432"
+        }
 }
 
 
